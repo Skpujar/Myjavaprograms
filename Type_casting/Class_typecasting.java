c663732fa77e8efa6b970ffed0571fbdfc4fc0b9
Skpujar <159273909+Skpujar@users.noreply.github.com>
2024-03-06 10:46:08 +0530

    Add files via upload
    Type casting programs

---
+++ Type_casting/Class_typecasting.java
@@ -0,0 +1,10 @@
+package Type_casting;
+
+public class Class_typecasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Type_casting/Shiva_Class.java
@@ -0,0 +1,22 @@
+package Type_casting;
+
+class object {
+
+	void sub () {
+		
+		
+	}
+}
+
+public class Shiva_Class extends object  {
+
+	void add() {
+
+	}
+
+	public static void main(String[] args) {
+		
+		object b1 = new Shiva_Class();
+	}
+
+}
